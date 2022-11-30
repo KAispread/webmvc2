33a04047e773a5507bdff765ae9a021fa954d918
KAispread <pain0121@naver.com>
2022-11-30 20:11:02 +0900

    session & cookie: 세션 쿠키 로그인 구현

---
+++ login-start/src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,53 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SessionManager {
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+    private final String SESSION_COOKIE_NAME = "mySessionId";
+
+    public void createSession(Object value, HttpServletResponse response) {
+        // sessionId 생성, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        // cookie
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(cookie);
+    }
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
+        }
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+}
