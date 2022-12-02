8e303ac48c80a51a147a780ca1e47d342f087fe1
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-12-02 13:45:26 +0900

    ErrorPage: sendError, ErrorPage 객체 생성, DispatcherType 등

--- exception/src/main/java/hello/exception/filter/LogFilter.java
+++ exception/src/main/java/hello/exception/filter/LogFilter.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 
 @Slf4j
 public class LogFilter implements Filter {
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         log.info("log filter init");
@@ -19,19 +18,15 @@ public class LogFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
-
         String uuid = UUID.randomUUID().toString();
-
         try {
-            log.info("REQUEST  [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
+            log.info("REQUEST [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
             chain.doFilter(request, response);
         } catch (Exception e) {
-            log.info("EXCEPTION {}", e.getMessage());
             throw e;
         } finally {
             log.info("RESPONSE [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
         }
-
     }
 
     @Override
--- exception/src/main/java/hello/exception/servlet/ServletController.java
+++ exception/src/main/java/hello/exception/servlet/ServletController.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 @Slf4j
 @Controller
-public class ServletExController {
+public class ServletController {
 
     @GetMapping("/error-ex")
     public void errorEx() {
@@ -17,17 +17,12 @@ public class ServletExController {
     }
 
     @GetMapping("/error-404")
-    public void error404(HttpServletResponse response) throws IOException {
-        response.sendError(404, "404 오류!");
-    }
-
-    @GetMapping("/error-400")
-    public void error400(HttpServletResponse response) throws IOException {
-        response.sendError(400, "400 오류!");
+    public void error404 (HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류 !");
     }
 
     @GetMapping("/error-500")
-    public void error500(HttpServletResponse response) throws IOException {
+    public void error500 (HttpServletResponse response) throws IOException {
         response.sendError(500);
     }
 }
